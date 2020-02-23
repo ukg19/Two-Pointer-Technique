3e9c495d1f0ffa5a3095cfd83a1c5c1129de68ef
Utkarsh Katiyar <utkarshkatiyar19@gmail.com>
2020-02-23 23:54:23 +0530

    Create sortColors.cpp

---
+++ sortColors.cpp
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/sort-colors/
+Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, 
+with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note: You are not suppose to use the library's sort function for this problem.
+
+Example:
+
+Input: [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Follow up:
+
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, 
+then 1's and followed by 2's.
+Could you come up with a one-pass algorithm using only constant space?
+*/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int sz = nums.size();
+        if(!sz)
+            return;
+        if(sz == 1)
+            return;
+        int f = 0, s = 0, t = sz -1;
+        while(s <= t){
+            if(nums[s] == 0){
+                swap(nums[s], nums[f]);
+                f++; 
+                s++;
+            }
+            else if(nums[s] == 2){
+                swap(nums[s], nums[t]);
+                t--; 
+            }
+            else
+                s++;
+        }
+    }
+};
